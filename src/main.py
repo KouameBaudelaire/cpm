d2e505a12e68065f917b6abc19785119431eb8c4
Donassongui <sorodonassongui0@gmail.com>
2026-04-24 13:57:47 +0000

    Add afficher_config function to display configuration
    Fix variable name in afficher_config function.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
+# src/main.py
 
+from src.config import DATABASE_URL, SECRET_KEY, DEBUG
+
+def afficher_config():
+    print(f"Base de données : {DATABASE_KEY}")
+    print(f"Debug activé : {DEBUG}")
